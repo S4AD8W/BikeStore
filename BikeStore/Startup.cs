f5ea401113e73cb78b9bb8bf4c6b360d595c170e
Igor Wieczorek <igorw645@gmail.com>
2018-11-21 19:51:02 +0100

    project configuration

--- BikeStore/Startup.cs
+++ BikeStore/Startup.cs
@@ -2,71 +2,67 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using BikeStore.Core.Domain.Repository;
-using BikeStore.Infrastructure.Mappers;
-using BikeStore.Infrastructure.Repository;
-using BikeStore.Infrastructure.Services;
+using Autofac;
+using Autofac.Extensions.DependencyInjection;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using BikeStore.Infrastructure.IoC;
 
-namespace BikeStore
-{
-  public class Startup
-  {
-    public Startup(IConfiguration configuration)
-    {
-      Configuration = configuration;
+namespace BikeStore {
+  public class Startup {
+
+    public IConfigurationRoot Configuration { get; }
+    public IContainer ApplicationContainer { get; private set; }
+
+    public Startup(IHostingEnvironment env) {
+
+      var builder = new ConfigurationBuilder()
+               .SetBasePath(env.ContentRootPath)
+               .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+               .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+               .AddEnvironmentVariables();
+      Configuration = builder.Build();
     }
 
-    public IConfiguration Configuration { get; }
+
 
     // This method gets called by the runtime. Use this method to add services to the container.
-    public void ConfigureServices(IServiceCollection services){
+    public IServiceProvider ConfigureServices(IServiceCollection services) {
 
-      // Add framework services.
-      services.AddScoped<IUserRepository, UserRepository>();
-      services.AddScoped<IUserService, UserService>();
-      services.AddSingleton(AutoMapperConfig.Initialize());
-
-      services.Configure<CookiePolicyOptions>(options =>
-      {
-        // This lambda determines whether user consent for non-essential cookies is needed for a given request.
-        options.CheckConsentNeeded = context => true;
-        options.MinimumSameSitePolicy = SameSiteMode.None;
-      });
+      services.AddMvc().AddJsonOptions(options => options.SerializerSettings.ContractResolver = new Newtonsoft.Json.Serialization.DefaultContractResolver());
 
 
-      services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+      var builder = new ContainerBuilder();
+      builder.Populate(services);
+      builder.RegisterModule(new ContainerModule(Configuration));
+      ApplicationContainer = builder.Build();
+
+      return new AutofacServiceProvider(ApplicationContainer);
     }
 
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-    public void Configure(IApplicationBuilder app, IHostingEnvironment env)
-    {
-      if (env.IsDevelopment())
-      {
+    public void Configure(IApplicationBuilder app, IHostingEnvironment env) {
+
+      if (env.IsDevelopment()) {
+        app.UseBrowserLink();
         app.UseDeveloperExceptionPage();
-      }
-      else
-      {
+      } else {
         app.UseExceptionHandler("/Home/Error");
-        app.UseHsts();
       }
 
-      app.UseHttpsRedirection();
       app.UseStaticFiles();
-      app.UseCookiePolicy();
 
-      app.UseMvc(routes =>
-      {
+
+      app.UseMvc(routes => {
+
+
         routes.MapRoute(
-                  name: "default",
-                  template: "{controller=Home}/{action=Index}/{id?}");
+          name: "default",
+          template: "{controller=Home}/{action=Index}/{id?}");
       });
+
     }
   }
 }
