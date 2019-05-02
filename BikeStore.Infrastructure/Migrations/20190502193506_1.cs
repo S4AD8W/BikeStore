8414a2abb70a25442bcf61381fc77d655f11fc21
Igor Wieczorek <igorw645@gmail.com>
2019-05-02 21:43:15 +0200

    work on  notyfication component

---
+++ BikeStore.Infrastructure/Migrations/20190502193506_1.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BikeStore.Infrastructure.Migrations
+{
+    public partial class _1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "NotificationStatus",
+                table: "ForksNotifications",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "NotificationStatus",
+                table: "ForksNotifications");
+        }
+    }
+}
---
+++ BikeStore/ViewsComponent/Information/NotificationCartInfo.cs
@@ -0,0 +1,32 @@
+﻿using BikeStore.Infrastructure.EF;
+using BikeStore.Models;
+using BikeStore.ViewModels.Information;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace BikeStore.ViewsComponent.Information {
+
+  [ViewComponent(Name = "NotificationCartInfo")]
+  public class NotificationCartInfo : ViewComponent {
+
+    public readonly BikeStoreContext mDB;
+    public NotificationCartInfo(BikeStoreContext xDB) {
+      mDB = xDB;
+    }
+
+    public async Task<IViewComponentResult> InvokeAsync() {
+
+      NotificationCartInfoVM pVM = new NotificationCartInfoVM();
+      pVM.ForkNotyficationCount = mDB.ForksNotifications.Count(x => x.NotificationStatus == core.Domain.Notification.NotificationStatusEnum.New);
+
+      return View("NotificationCartInfo",pVM);
+    }
+  }
+}
