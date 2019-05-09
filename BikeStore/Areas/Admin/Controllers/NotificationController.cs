bcdd8ee297e4be848b2b9c9537fa71191671a0d7
Igor Wieczorek <igorw645@gmail.com>
2019-05-09 22:03:35 +0200

    Work on fork notyfication

--- BikeStore/Areas/Admin/Controllers/NotificationController.cs
+++ BikeStore/Areas/Admin/Controllers/NotificationController.cs
@@ -5,8 +5,10 @@ using System.Threading.Tasks;
 using AutoMapper;
 using BikeStore.Areas.Admin.ViewModel;
 using BikeStore.Controllers;
+using BikeStore.core.Domain.Notification;
 using BikeStore.Infrastructure.Commands;
 using BikeStore.Infrastructure.EF;
+using BikeStore.ViewsModels;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -15,15 +17,31 @@ namespace BikeStore.Areas.Admin.Controllers {
   public class NotificationController : BikeStoreControllerBaseController {
 
     private readonly BikeStoreContext mDB;
-
+    int mPageSize = 1;
     public NotificationController(ICommandDispatcher xCommandDispatcher, IMapper xMapper, BikeStoreContext xDB)
       : base (xCommandDispatcher,xMapper) {
       mDB = xDB;
     }
-    public async Task<IActionResult> Fork() {
+    public async Task<IActionResult> Fork(NotificationStatusEnum xStatus, int productpage = 1) {
 
       AllForkVM pVM = new AllForkVM();
-      pVM.Forks = await mDB.ForksNotifications.ToListAsync(); 
+      pVM.Forks = await mDB.ForksNotifications.ToListAsync();
+
+      pVM.Forks = await mDB.ForksNotifications
+                    .Where(p => xStatus != NotificationStatusEnum.New || p.NotificationStatus == xStatus)
+                    //.OrderBy(p => p.ProductID)
+                    .Skip((productpage - 1) * mPageSize)
+                    .Take(mPageSize).ToListAsync();
+
+      pVM.PagingInfo = new PagingInfo {
+        CurrentPage = productpage,
+        ItemsPerPage = mPageSize,
+        TotalItems = xStatus != NotificationStatusEnum.New ?
+                        mDB.ForksNotifications.Count() :
+                        mDB.ForksNotifications.Where(e =>
+                            e.NotificationStatus == xStatus).Count()
+      };
+      
 
       return View(pVM);
     }
--- BikeStore/Areas/Admin/ViewModel/AllForkVM.cs
+++ BikeStore/Areas/Admin/ViewModel/AllForkVM.cs
@@ -1,4 +1,5 @@
 ﻿using BikeStore.core.Domain.Notification;
+using BikeStore.ViewsModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,5 +9,7 @@ namespace BikeStore.Areas.Admin.ViewModel {
   public class AllForkVM {
 
     public IEnumerable<ForkNotification> Forks { get; set; }
+    public PagingInfo PagingInfo { get; set; }
+
   }
 }
