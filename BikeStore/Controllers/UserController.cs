eeaa082827f979d8b4b3021354d1214e183c060e
Igor Wieczorek <igorw645@gmail.com>
2019-05-11 15:25:32 +0200

    Wok on user account views

---
+++ BikeStore/Controllers/UserController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using BikeStore.Infrastructure.Commands;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BikeStore.Controllers {
+  public class UserController : BikeStoreControllerBaseController{
+
+    public UserController(ICommandDispatcher xCommandDispatcher, IMapper xMapper)
+           : base(xCommandDispatcher, xMapper) {
+
+    } 
+
+    
+    public IActionResult UserAccount() {
+      return View();
+    }
+
+    public IActionResult ForkNotification() {
+
+      return View();
+    }
+  }
+}
