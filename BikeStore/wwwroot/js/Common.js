d70ef3525ac688c8dd690808b6c9bbcf998bc401
Igor Wieczorek <igorw645@gmail.com>
2019-11-21 21:56:31 +0100

    Working on the store

--- BikeStore/wwwroot/js/Common.js
+++ BikeStore/wwwroot/js/Common.js
@@ -6,42 +6,6 @@
 
 var JsCommon = {
 
-  DXToastType: {
-    Info: "info",
-    Custom: "custom",
-    Error: "error",
-    Success: "success",
-    warning: "warning"
-  },
-
-  GetPopUp: function (xPopUpId) {
-    //funkcja pokazująca popupa 
-    //xPopUpId - id okienka 
-
-    document.getElementById(xPopUpId).classList.remove("hidden");
-
-  },
-
-  ClosePopUp: function (xPopUpId) {
-    //funkcja zamykająca okienko popup
-    //xPopUpId - id okienka 
-
-    let pElement = document.getElementById(xPopUpId);
-
-    if (!pElement.classList.contains("hidden")) {
-      pElement.classList.add("hidden");
-    }
-
-  },
-
-  ResetForm: function (xIdForm) {
-    //funkcja restująca formularz
-    //xIdForm - id formularza do zresetowania 
-
-    document.getElementById(xIdForm).rset();
-
-  },
-
   SetCookie: function (cname, cvalue, exdays) {
 
     var d = new Date();
@@ -70,47 +34,13 @@ var JsCommon = {
     return "";
 
   },
+	SetCssClassForElement(xElement, xCssClass) {
+		//funkcja dodająca klase css dla elementu html
 
-  DXShowToast: function (xText, xDisplayTime_Optional, xType_Optional) {
-    //funkcja pokazująca toast 
-    //xText - tekst który ma być wyświetlony
-    //xDisplayTime_Optional - czas na jaki ma być pokazany toast
-    //xType_Optional -  rodzaj stylu wyświetlanego toast
-
-    let pDisplayTime = 1500,
-      pTypeToast = JsCommon.DXToastType.Info;
-
-    if (xDisplayTime_Optional !== undefined) { pDisplayTime = xDisplayTime_Optional; }
-    if (xType_Optional !== undefined) { pTypeToast = xType_Optional; }
-
-    $("#DXToastCommon").dxToast({
-      message: xText,
-      type: pTypeToast, //'custom' | 'error' | 'info' | 'success' | 'warning'
-      displayTime: 1500
-    });
-
-    $("#DXToastCommon").dxToast("show");
-
-  },
-
-  CustomDropDawn(xEvent) {
-    //funcka pokazująca kolory w stylu dropdawn list
-    //xElement - aktualny element
-    //xSetVisible_ID - identfikator elementu który ma zostać pokazanny
-
-    let pDropDown = document.getElementById(xEvent.currentTarget.dataset.target); //pobranie elementu który ma być pokazany
-
-    if (!pDropDown.classList.contains("dropdown-content-Active")) {// sprawdzenie czy przypadkiem wybrany element niejest już widoczny
-      pDropDown.classList.add("dropdown-content-Active");   //dodanie dla elementu klase active
-      //↓sprawdzenie czy strzałka ma ustawioną odpowiednią klase
-      if (!xEvent.currentTarget.children[0].childNodes[1].classList.contains("ImgDropDownListActive")) { xEvent.currentTarget.children[0].childNodes[1].classList.add("ImgDropDownListActive"); }
-    } else {
-      pDropDown.classList.remove("dropdown-content-Active"); //usuniecie klasy aktive ukrycie elementu
-      //↓ spradzenie czy strzłka jest ustwiona w odpowiednią strone
-      if (xEvent.currentTarget.children[0].childNodes[1].classList.contains("ImgDropDownListActive")) { xEvent.currentTarget.children[0].childNodes[1].classList.remove("ImgDropDownListActive"); }
-    }
-
-  },
+		if (!xElement.classList.contains(xCssClass)) {
+			xElement.classList.add(xCssClass);
+		}
+	},
 
   AutoGrow(event) {
     //funkcja powudująca automatyczne rośniecie elementu 
@@ -169,34 +99,6 @@ var JsCommon = {
     return pReturnArray;
   },
 
-  MarkElemntAsSelected(xIdentiti, xCssClass) {
-    //funkcja zaznaczająca wybrany elemnt na stronie jako wybrany
-    //xIdentiti - identyfikator elementu kóry jest wybarany
-    //xCssClass - unikalna klasa css dla grupy elemntów z których jest jeden wybierany
-
-    let pElements = document.getElementsByClassName(xCssClass);
-
-    for (var i = 0; i < pElements.length; i++) {
-      pElements[i].classList.remove("DisableElementDesigning");
-      if (pElements[i].attributes.psidentiti.value === xIdentiti) continue;
-      if (pElements[i].classList.contains("DisableElementDesigning")) continue;
-      pElements[i].classList.add("DisableElementDesigning");
-    }
-
-  },
-
-  RefreshLeftChoiceInfoMenu() {
-    //funkcja odsiwierzająca pływające menu po lewej stronie
-
-    let pMenuContainer = document.getElementById("FloatingLeftMenu");
-
-    $.post("/Designing/RefreshLeftChoiceInfoMenu")
-      .done(function (xData) {
-        pMenuContainer.innerHTML = xData;
-      })
-      .fail(() => window.location.reload());
-
-  },
 
   SetDisableAtrribute(xElement) {
     //funkcja ustawiająca dla wybranego elementu atrybut disabled
@@ -218,17 +120,6 @@ var JsCommon = {
 
   },
 
-  CheckCurrentDesigningModule(xToMatchSubStrUri) {
-    //funkcja sprawdzająca na podstawie Uri aktywny tryp projektowania 
-    //xToMatchSubStrUri - string do prównania z Uri
-
-    if (window.location.pathname.includes(xToMatchSubStrUri)) {  //sprawdzenie trybu projektowania na podstawie uri
-      return true;
-    }
-
-    return false;
-
-  },
 
   GoToElemenTopView(xId, xElement) {
     //funkcja scrolująca dokument do wskazanego elementu
@@ -249,108 +140,6 @@ var JsCommon = {
 
   },
  
-  GoToProfileSystem() {
-    //funkcja odpowiadająca za przejście do systemu profilowego
-
-    let pProfileSystemRow = document.getElementById("ProfileSystem_Row");
-
-    $.get("ProfileSystems")
-      .done(xData => {
-        pProfileSystemRow.innerHTML = xData;
-        JsCommon.RefreshLeftChoiceInfoMenu();
-        JsCommon.GoToElemenTopView("ProfileSystem_Row");
-        JsDesigning.ProfileSystems.PrepareToView();
-        $("#LPProjectPattern").dxLoadPanel("instance").hide();
-      })
-      .fail(xData => {
-
-      });
-
-  },
-
-  GoToDimensions() {
-    //funkcja przechądząca do karty wymiarowania
-
-    $.get("Dimensions")
-      .done(xData => {
-        debugger;
-        JsDesigning.Dimensions.PrepareToView(xData);
-        JsCommon.RefreshLeftChoiceInfoMenu();
-      })
-      .fail(xData => {
-        alert("fail from go to dimensions");
-      });
-  },
-
-  GoToDetailsConfiguration() {
-    //funkcja przechądząca do karty detali konfiguracji 
-
-    $.get("Wings")
-      .done(xData => {
-        let pWingsContainer = document.getElementById("WingsConfiguration");
-        pWingsContainer.innerHTML = xData;
-        $("#WingsConfigurationTab").tab('show');
-        JsDesigning.WingDesigning.PrepareToView();
-        JsCommon.GoToElemenTopView("WingsConfiguration");
-      })
-      .fail(xData => {
-
-      });
-
-  },
-
-  GoToSummary() {
-    //funkcja przechodząca do karty podsumowania
-    
-    $.get("Summary")
-      .done(xData => {
-        document.getElementById("Summary").innerHTML=xData;
-        JsDesigning.Summary.PrepareView();
-        $("#WindowSummary").tab('show');
-      })
-      .fail(xData => {
-
-      });
-  },
-
-  GoToOpeningSide() {
-    //funkcja przechodząca do wyboru sposobu otwierania 
-    
-    $.get("OpeningSide")
-      .done(xData => {
-        document.getElementById("OpeningSideRow").innerHTML = xData;
-        JsDesigning.OpeningSide.PrepareView();
-        JsCommon.RefreshLeftChoiceInfoMenu();
-      })
-      .fail(xData => {
-
-      });
-
-  },
-
-  DownloadViewPanels() {
-    //funkcja pobierająca widok z panelami 
-
-    $.get("Panel")
-      .done(xData => {
-        JsDesigning.Panels.PrepareToView(xData);
-      })
-      .fail(xData => {
-        return false;
-      });
-  },
-
-  DownloadViewAdditives() {
-    //funkcja pobierająca widok z dodatkami
-
-    $.get("Additives")
-      .done(xData => {
-        JsDesigning.Additives.PrepareToView(xData);
-      })
-      .fail(xData => {
-        return false;
-      });
-  }
 
 };
 
